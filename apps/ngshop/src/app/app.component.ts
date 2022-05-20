0c410fc4dbd7b2bad1a4e9b7e0b1f0c4fde5689f
Amr Elzafarani <amrelza3frany@gmail.com>
2022-05-20 18:56:06 +0200

    Add Cart when open app

--- apps/ngshop/src/app/app.component.ts
+++ apps/ngshop/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { CartService } from '@eshop/orders';
 
 @Component({
   selector: 'ngshop-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+
+  constructor(private cartService: CartService){
+    this.cartService.initCartLocalStorage();
+  }
   title = 'ngshop';
 }
