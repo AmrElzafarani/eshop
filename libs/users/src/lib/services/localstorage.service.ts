ff8958e1461f7907ada043ea4240828c960ab979
Amr Elzafarani <amrelza3frany@gmail.com>
2022-05-23 14:10:22 +0200

    check token validity and get user id

--- libs/users/src/lib/services/localstorage.service.ts
+++ libs/users/src/lib/services/localstorage.service.ts
@@ -5,8 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class LocalstorageService {
 
-  constructor() { }
-
   setToken(data: any) {
     localStorage.setItem('user', data)
   }
@@ -17,5 +15,33 @@ export class LocalstorageService {
 
   removeToken() {
     localStorage.removeItem('user')
+  }
+
+  isValidToken () {
+    const token = this.getToken();
+    if(token) {
+      const tokenDecoded = JSON.parse(atob(token.split('.')[1]));
+      return !this._tokenExpired(tokenDecoded.exp)
+    } else {
+      return false;
+    }
+  }
+
+  getUserIdFromToken() {
+    const token = this.getToken();
+    if (token) {
+      const tokenDecode = JSON.parse(atob(token.split('.')[1]));
+      if (tokenDecode) {
+        return tokenDecode.userId;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  private _tokenExpired(expirationDate: number): boolean {
+    return Math.floor(new Date().getTime() / 1000) >= expirationDate;
   }
 }
